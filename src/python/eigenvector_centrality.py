1a6b28ef4059f6436c3fa9795369b29eb3d02152
Txangel <txangel@acm.org>
2024-12-04 01:17:56 +0000

    done

--- src/python/eigenvector_centrality.py
+++ src/python/eigenvector_centrality.py
@@ -7,52 +7,75 @@ class Graph:
     def __init__(self, w, h):
         self.graph = [[0 for x in range(w)] for y in range(h)]
 
-    def add_edge(self, u, v):
+    def add_edge(self, in_u, in_v):
+        u = in_u - 1
+        v = in_v - 1
         if u not in self.graph:
             self.graph[u][v] = 1
 
     def eigenvector_centrality(self):
-        new_eigencentres = [0 for x in range(len(self.graph))]
-        old_eigencentres = [0 for x in range(len(self.graph))]
-        for iteration in range(20):
+        new_eigencentres = self.init_eigenvector()
+        old_eigencentres = self.init_eigenvector()
+        for iteration in range(5000):
             sum = 0
             for i in range(len(self.graph)):
                 score = 0
                 for j in range(len(self.graph)):
-                    score += self.graph[i][j]*(old_eigencentres[j] if old_eigencentres[j] != 0 else 1)
+                    score += self.graph[i][j]*(old_eigencentres[j]['score'] if old_eigencentres[j]['score'] != 0 else 1)
                     sum += score
-                new_eigencentres[i] = score
-            new_eigencentres = [x/sum for x in new_eigencentres]
-            print("Iteration{} centers: {}".format(iteration,new_eigencentres))
-            for i in range(len(old_eigencentres)):
-                old_eigencentres[i] = new_eigencentres[i]
+                new_eigencentres[i]['score'] = score
+            for x in new_eigencentres:
+                x['score'] = x['score']/sum
 
-        max = 0
-        max_index = 0
-        for x in range(len(new_eigencentres)):
-            if new_eigencentres[x] > max:
-                max = new_eigencentres[x]
-                max_index = x
+            diff = 0
+            for i,x in enumerate(old_eigencentres):
+                diff += abs(x['score'] - new_eigencentres[i]['score'])
+            if diff < 0.000000001:
+                break
 
-        print("The most central node is: {} with a centrality of: {}".format(max_index, max))
+            for i,x in enumerate(old_eigencentres):
+                x['score'] = new_eigencentres[i]['score']
 
-graph = None
+
+        return sorted(new_eigencentres, key=lambda x: x['score'], reverse=True)
+
+    def init_eigenvector(self):
+        eigenvector = []
+        for i in range(1, len(self.graph)+1, 1):
+            eigenvector.append({'name': self.index[i] if i in self.index else 'UNKNOWN', 'score': 0})
+        return eigenvector
+
+    def set_index(self, index):
+        self.index = index
+
 
 def main():
     args = configure_arguments()
     graph = load_graph(args)
-    graph.eigenvector_centrality()
+    eigenvector_centers = graph.eigenvector_centrality()
+
+    for x in eigenvector_centers:
+        print("{}, {}".format(x['name'], x['score']))
 
 def load_graph(args):
+
     with open(args.nodes) as nodes:
         reader = csv.DictReader(nodes)
-        dimension = len(list(reader)) + 1
+        dimension = 0
+        index = {}
+        for row in reader:
+            id = int(row['id'])
+            index[id] = row['name']
+            if  id > dimension:
+                dimension = id
+
         graph = Graph(dimension, dimension)
+        graph.set_index(index)
     with open(args.edges) as edges:
         reader = csv.DictReader(edges)
         for row in reader:
-            graph.add_edge(int(row['station1']) - 1, int(row['station2']) - 1)
-            graph.add_edge(int(row['station2']) - 1, int(row['station1']) - 1)
+            graph.add_edge(int(row['station1']), int(row['station2']))
+            graph.add_edge(int(row['station2']), int(row['station1']))
     return graph
 
 
